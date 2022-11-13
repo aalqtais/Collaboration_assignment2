64bf5de90621c1afb886fb598f7fc3b06ac84dba
Ahmad09988 <Ahmada.salim98@gmail.com>
2022-11-13 19:07:29 +0200

    Develop made some changes.

--- Gitflow/src/Main.java
+++ Gitflow/src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Making gitflow method");
+
+        System.out.println("making the Develop branch");
+        System.out.println("making some changes");
+
     }
 }
